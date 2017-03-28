1942f653f704148dea878aa7cf5c2abcceadfdaf
guanwenchen0304 <guanwen0304@gmail.com>
2017-03-29 02:08:38 +0800

    Add files via upload

---
+++ test5.py
@@ -0,0 +1,11 @@
+a=100
+if(a>100):
+    print("a大於100")
+else:
+    print("a小於100")
+s = input("請輸入數字:")
+print("請輸入數字:",int(s))
+if(int(s)>100):
+    print(int(s),"大於100")
+else:
+    print(int(s),"小於100")
